f6aea60e31f2bf6c3583757197216cc5c2c589be
Henry Leung <henryskyleung@gmail.com>
2023-03-13 01:58:57 -0400

    use pytest dependency to order test

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,12 +1,14 @@
+import pytest
 import mygaiadb
 from mygaiadb.data import download, compile
 
 
-class Tests:
-    def test_download(self):
-        download.download_gaia_source(test=True)
-        download.download_2mass_best_neightbour(test=True)
-        download.download_allwise_best_neightbour(test=True)
+def test_download():
+    download.download_gaia_source(test=True)
+    download.download_2mass_best_neightbour(test=True)
+    download.download_allwise_best_neightbour(test=True)
 
-    def test_compile(self):
-        compile.compile_gaia_sql_db(do_indexing=False)
+
+@pytest.mark.dependency(depends=["test_download"])
+def test_compile():
+    compile.compile_gaia_sql_db(do_indexing=False)
