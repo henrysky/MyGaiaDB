be402e6055b7aeff032f753b2ec68604e89dca3a
Henry Leung <henryskyleung@gmail.com>
2023-03-13 08:57:45 -0400

    use pytest order to order test

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,19 +3,19 @@ import mygaiadb
 from mygaiadb.query import LocalGaiaSQL
 from mygaiadb.data import download, compile
 
-
+@pytest.mark.order(1)
 def test_download():
     download.download_gaia_source(test=True)
     download.download_2mass_best_neightbour(test=True)
     download.download_allwise_best_neightbour(test=True)
 
 
-@pytest.mark.dependency(depends=["test_download"])
+@pytest.mark.order(2)
 def test_compile():
     compile.compile_gaia_sql_db(do_indexing=False)
 
 
-@pytest.mark.dependency(depends=["test_download", "test_compile"])
+@pytest.mark.order(3)
 def test_query():
     localdb = LocalGaiaSQL()
     print(localdb.query("""SELECT * FROM gaiadr3.gaia_source LIMIT 10"""))
