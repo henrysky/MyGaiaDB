eb70f7bc20c422d1d8aad8551d40d0520fdaf805
Henry Leung <henryskyleung@gmail.com>
2023-03-15 18:14:33 -0400

    fix cleanup script

--- tests/cleanup.py
+++ tests/cleanup.py
@@ -3,7 +3,7 @@ import sys
 import stat
 import pathlib
 
-if len(sys.arg) > 0:
+if len(sys.argv) > 0:
     if sys.argv[1] == "i_swear_i_am_on_gh_action":  # make sure no accidential execution
         astro_data_path = os.getenv("MY_ASTRO_DATA")
         astro_data_path = pathlib.Path(astro_data_path).expanduser()
