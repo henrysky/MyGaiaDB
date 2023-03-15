cf37be116d9bc1c66abeb87d0f9e6df9e1d6693a
Henry Leung <henryskyleung@gmail.com>
2023-03-15 10:33:45 -0400

    relax assert tolerance

--- tests/test_ext.py
+++ tests/test_ext.py
@@ -10,9 +10,9 @@ def test_c_exts():
     c = sqlite3.connect(":memory:")
     LocalGaiaSQL._load_sqlite3_ext(c)
     # test geometrical functions
-    assert c.execute("""SELECT COS(1.)""").fetchall() == np.cos(1.)
-    assert c.execute("""SELECT SIN(1.)""").fetchall() == np.sin(1.)
-    assert c.execute("""SELECT TAN(1.)""").fetchall() == np.tan(1.)
+    np.testing.assert_approx_equal(c.execute("""SELECT COS(1.)""").fetchall()[0][0], np.cos(1.), 14)
+    np.testing.assert_approx_equal(c.execute("""SELECT SIN(1.)""").fetchall()[0][0], np.sin(1.), 14)
+    np.testing.assert_approx_equal(c.execute("""SELECT TAN(1.)""").fetchall()[0][0], np.tan(1.), 14)
 
     # merak and dubhe for all amateur stargazer
     np.testing.assert_approx_equal(c.execute("""SELECT DISTANCE(165.458, 56.3825, 165.933, 61.7511)""").fetchall()[0][0], 5.37411, 4)
